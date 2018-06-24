1a5dcbe8d4bed1031082c17084a47d04a2858cde
kimyoungsun <kyssmart@naver.com>
2018-06-24 21:20:42 +0900

    move test
    move test

---
+++ Assets/test/MoveTest/EnemySpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoveTest
+{
+	public enum MOVE_TYPE  { UpdateType, FixedType };
+    public class EnemySpawner : MonoBehaviour
+	{
+		float waitTime;
+		public float WAIT_TIME = 2f;
+        public List<Transform> spawnPoint 	= new List<Transform>();
+		public List<GameObject> prefabObject = new List<GameObject>();
+
+
+		public MOVE_TYPE mode = MOVE_TYPE.UpdateType;
+		public float speed = 6f;
+		public UILabel modeLabel, speedLabel;
+
+
+		public void InvokeChangeMode(){
+			switch(mode){
+			case MOVE_TYPE.UpdateType:
+				mode = MOVE_TYPE.FixedType;
+				break;
+			case MOVE_TYPE.FixedType:
+				mode = MOVE_TYPE.UpdateType;
+				break;
+			}
+			modeLabel.text = mode.ToString ();
+		}
+
+		public void InvokeChangeSpeed(){
+			speed++;
+			if (speed >= 10f) {
+				speed = 5f;
+			}
+
+			speedLabel.text = speed.ToString ();
+		}
+
+        void Update()
+        {
+            if(Time.time > waitTime)
+            {
+				waitTime = Time.time + WAIT_TIME;
+				for(int i = 0, iMax = prefabObject.Count; i < iMax; i++)
+                {
+					MoveTest _mt = PoolManager.ins.Instantiate (prefabObject [i], spawnPoint [i].position, Quaternion.identity).GetComponent<MoveTest>();
+					_mt.MoveMove (mode, speed);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/test/MoveTest/MoveTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoveTest
+{
+    public class MoveTest : MonoBehaviour
+    {
+		public MOVE_TYPE mode = MOVE_TYPE.UpdateType;
+		public float speed = 6f;
+		Transform trans;
+		public float limitY = -10f;
+
+		void Start(){
+			trans = transform;
+		}
+
+
+		public void MoveMove(MOVE_TYPE _mode, float _speed){
+			mode 	= _mode;
+			speed 	= _speed;
+		}
+
+        // Update is called once per frame
+        void Update()
+        {
+			if (mode == MOVE_TYPE.UpdateType) {
+				trans.Translate (Vector3.down * speed * Time.deltaTime);
+				if (trans.position.y < limitY) {
+					gameObject.SetActive (false);
+				}
+			}
+        }
+
+		void FixedUpdate(){
+			if (mode == MOVE_TYPE.FixedType) {
+				trans.Translate (Vector3.down * speed * Time.fixedDeltaTime);
+				if (trans.position.y < limitY) {
+					gameObject.SetActive (false);
+				}
+			}
+		}
+    }
+}
