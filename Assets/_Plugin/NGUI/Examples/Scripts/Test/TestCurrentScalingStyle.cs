cbf5f45963e42cb6de1e25848f2f19cfce52333a
kimyoungsun3 <kyssmart@naver.com>
2018-06-27 22:38:06 +0900

    구이 Test
    구이 Test

---
+++ Assets/_Plugin/NGUI/Examples/Scripts/Test/TestCurrentScalingStyle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestCurrentScalingStyle : MonoBehaviour {
+	public UIRoot root;
+	public UILabel label;
+
+
+	void Start () {
+		label.text = "" + root.scalingStyle;
+	}
+
+	public void InvokeChangeScene0(){
+		SceneManager.LoadScene (0);
+	}
+
+	public void InvokeChangeScene1(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void InvokeChangeScene2(){
+		SceneManager.LoadScene (2);
+	}
+}
